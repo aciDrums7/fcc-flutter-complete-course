7d76b33799117a14df420a281a88e08c8bde183d
aciDrums7 <edoardo.carradori.96@gmail.com>
2024-05-25 19:05:39 +0200

    feat(chapter4): :sparkles: Chapter 4 - Dart Control Statements and Collections

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-String getFullName(String firstName, String lastName) {
-  return '$firstName $lastName';
+void test() {
+  var person = {
+    'name': 'John',
+    'age': 30,
+  };
+  print(person);
+  person['name'] = 'Doe';
+  print(person);
 }
-
-String getFullNameArrowFn(String firstName, String lastName) =>
-    '$firstName $lastName';
 
 void main() {
   runApp(const MyApp());
@@ -17,6 +20,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    test();
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
